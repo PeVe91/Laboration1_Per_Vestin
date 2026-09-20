dc338420dc998e454d1085771b1e9814ba3757d6
PeVe91 <321365708+PeVe91@users.noreply.github.com>
2026-09-20 15:01:30 +0200

    Update Library.java
    Ändrat expand-metodernas synlighet till private då de endast ska anropas av andra metoder som redan finns i Library och aldrig anropas direkt utifrån. Lagt till array av typer Loan och skapat en räknare för lån.

--- src/main/java/library/Library.java
+++ src/main/java/library/Library.java
@@ -4,14 +4,17 @@ public class Library {
     private final String name;
     private Book[] books;
     private Member[] members;
+    private Loan[] loans;
     private int bookCount = 0;
     private int memberCount = 0;
+    private int loanCount = 0;
 
     //------------------CONSTRUCTOR------------------
     public Library(String name, int initialBookCap, int initialMemberCap) {
         this.name = name;
         this.books = new Book[initialBookCap];
         this.members = new Member[initialMemberCap];
+        this.loans = new Loan[initialBookCap];
     }
 
     //--------------------METHODS--------------------
@@ -30,13 +33,13 @@ public class Library {
         books[bookCount++] = book;
     }
 
-    public void expandMembersArray() {
+    private void expandMembersArray() {
         Member[] newMembers = new Member[members.length * 2];
         System.arraycopy(members, 0, newMembers, 0, memberCount);
         members = newMembers;
     }
 
-    public void expandBooksArray() {
+    private void expandBooksArray() {
         Book[] newBooks = new Book[books.length * 2];
         System.arraycopy(books, 0, newBooks, 0, bookCount);
         books = newBooks;
